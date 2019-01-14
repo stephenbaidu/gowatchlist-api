a7af4474f3f2907c743133fa06b73c4a6cd66a92
Stephen Baidu <stephenbaidu@gmail.com>
2019-01-14 21:11:17 +0000

    Reduce cron time for watchlist_scheduler_job

--- app/jobs/watchlist_scheduler_job.rb
+++ app/jobs/watchlist_scheduler_job.rb
@@ -1,6 +1,4 @@
 class WatchlistSchedulerJob < ApplicationJob
-  queue_as :watch_schedulers
-
   def perform(*args)
     Watchlist.ready_for_execution.each do |watchlist|
       WatchlistJob.perform_later(watchlist.id)
